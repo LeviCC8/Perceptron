b8879f131bc9db8ec22a6d5d3f1e7d0495ea04ff
LeviCC8 <leviccxd@gmail.com>
2019-04-14 10:46:51 -0300

    adicionada superfície de decisão

--- rede.py
+++ rede.py
@@ -7,7 +7,7 @@ dataNumber = 150
 dataNumber08 = int(0.8*dataNumber)
 
 mydata = np.genfromtxt('iris.txt', delimiter = ",")
-X = mydata[:, :-1].reshape(-1, 4)
+X = mydata[:, :2].reshape(-1, 2)
 Y = mydata[:, -1].reshape(-1, 1)
 
 hitRatesPlot = []
@@ -16,7 +16,7 @@ confusionMatrix = []
 def train(samples, tests, learningRate, eras):
 	totalError = 1
 	hitRatesPlotN = []
-	w = np.random.rand(1,5)
+	w = np.random.rand(len(X[0]) + 1)
 	for n in range(eras):
 		if totalError == 0:
 			break;
@@ -49,8 +49,10 @@ def test(w, tests):
 	hitRate = (TP + TN)/(TP + TN + FP + FN)
 	return hitRate
 
+
 def accuracyAndStandardDeviation(realizations):
 	hitRates = []
+	weightVectors = []
 	for a in range(realizations):
 		lista = np.random.permutation(dataNumber)
 		samples = lista[:dataNumber08]
@@ -58,13 +60,19 @@ def accuracyAndStandardDeviation(realizations):
 		w = train(samples, tests, 0.1, 100)
 		hitRate = test(w, tests)
 		hitRates.append(hitRate)
+		weightVectors.append(w)
 	accuracy = np.mean(hitRates) 
 	standardDeviation = np.std(hitRates)
-	informations = {"accuracy": accuracy, "standardDeviation": float(standardDeviation)}
+	bestWeightVector = reduce(lambda a, b: a if (abs(test(a, tests) - accuracy) < abs(test(b, tests) - accuracy)) else b,weightVectors)
+	informations = {"accuracy": accuracy, "standardDeviation": float(standardDeviation), "w": bestWeightVector}
 	return informations
 
-informations = accuracyAndStandardDeviation(20)
+def functionPlot(x1):
+	w = informations['w']
+	x2 = (-w[1]*x1 + w[0])/w[2]
+	return x2
 
+informations = accuracyAndStandardDeviation(20)
 q = reduce(lambda a, b: a if ((abs((confusionMatrix[a][0][0] + confusionMatrix[a][1][1])/dataNumber*0.2 - informations["accuracy"])) < (abs((confusionMatrix[b][0][0] + confusionMatrix[b][1][1])/dataNumber*0.2 - informations["accuracy"]))) else b, range(len(confusionMatrix)))
 greatConfusionMatrix = confusionMatrix[q]
 
@@ -72,9 +80,23 @@ print("greatConfusionMatrix: \n{}".format(greatConfusionMatrix))
 print("accuracy: {:.4f}".format(informations["accuracy"]))
 print("standardDeviation: {:.4f}".format(informations["standardDeviation"]))
 
+#Plot hitRate X Eras
+plt.figure(1)
 num = reduce(lambda a, b: a if (abs(hitRatesPlot[a][-1] - informations["accuracy"]) < abs(hitRatesPlot[b][-1] - informations["accuracy"]) ) else b, range(len(hitRatesPlot)))
-plot = hitRatesPlot[num]
-plt.plot(range(len(plot)), plot, 'r')
-plt.xlabel("Época")
-plt.ylabel("Taxa de acerto")
+bestHitRate = hitRatesPlot[num]
+plt.plot(range(len(bestHitRate)), bestHitRate, 'r')
+plt.xlabel("Era")
+plt.ylabel("Hit rate")
+
+#Plot Hyperplane
+plt.figure(2)
+hyperplaneX = np.linspace(X[:, :1].min() - 1, X[:, :1].max() + 1, 1000)
+hyperplaneY = [functionPlot(a) for a in hyperplaneX]
+x1High = [(X[:, :1][a] if Y[a] == 1 else None) for a in range(len(Y))]
+x2High = [(X[:, 1:2][a] if Y[a] == 1 else None) for a in range(len(Y))]
+x1Low = [(X[:, :1][a] if Y[a] == 0 else None) for a in range(len(Y))]
+x2Low = [(X[:, 1:2][a] if Y[a] == 0 else None) for a in range(len(Y))]
+plt.plot(hyperplaneX, hyperplaneY, 'k', x1High, x2High, 'r^', x1Low, x2Low, 'bo')
+plt.xlabel("x1")
+plt.ylabel("x2")
 plt.show()
