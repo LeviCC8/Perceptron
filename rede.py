51a7d8a1dbb4c82dc861c6a0eec28c729523c35b
LeviCC8 <leviccxd@gmail.com>
2019-04-19 12:28:38 -0300

    mudança para um tratamento matricial

--- rede.py
+++ rede.py
@@ -64,3 +64,55 @@ W = train(samples, tests, 0.1, 100)
 hitRate = test(W, tests)
 print(W)
 print(hitRate)
+'''
+def accuracyAndStandardDeviation(realizations):
+	hitRates = []
+	weightVectors = []
+	for a in range(realizations):
+		lista = np.random.permutation(dataNumber)
+		samples = lista[:dataNumber08]
+		tests = lista[dataNumber08:]
+		w = train(samples, tests, 0.1, 100)
+		hitRate = test(w, tests)
+		hitRates.append(hitRate)
+		weightVectors.append(w)
+	accuracy = np.mean(hitRates) 
+	standardDeviation = np.std(hitRates)
+	bestWeightVector = reduce(lambda a, b: a if (abs(test(a, tests) - accuracy) < abs(test(b, tests) - accuracy)) else b,weightVectors)
+	informations = {"accuracy": accuracy, "standardDeviation": float(standardDeviation), "w": bestWeightVector}
+	return informations
+
+def functionPlot(x1):
+	w = informations['w']
+	x2 = (-w[1]*x1 + w[0])/w[2]
+	return x2
+
+informations = accuracyAndStandardDeviation(20)
+q = reduce(lambda a, b: a if ((abs((confusionMatrix[a][0][0] + confusionMatrix[a][1][1])/dataNumber*0.2 - informations["accuracy"])) < (abs((confusionMatrix[b][0][0] + confusionMatrix[b][1][1])/dataNumber*0.2 - informations["accuracy"]))) else b, range(len(confusionMatrix)))
+greatConfusionMatrix = confusionMatrix[q]
+
+print("greatConfusionMatrix: \n{}".format(greatConfusionMatrix))
+print("accuracy: {:.4f}".format(informations["accuracy"]))
+print("standardDeviation: {:.4f}".format(informations["standardDeviation"]))
+
+#Plot hitRate X Eras
+plt.figure(1)
+num = reduce(lambda a, b: a if (abs(hitRatesPlot[a][-1] - informations["accuracy"]) < abs(hitRatesPlot[b][-1] - informations["accuracy"]) ) else b, range(len(hitRatesPlot)))
+bestHitRate = hitRatesPlot[num]
+plt.plot(range(len(bestHitRate)), bestHitRate, 'r')
+plt.xlabel("Era")
+plt.ylabel("Hit rate")
+
+#Plot Hyperplane
+plt.figure(2)
+hyperplaneX = np.linspace(X[:, :1].min() - 1, X[:, :1].max() + 1, 1000)
+hyperplaneY = [functionPlot(a) for a in hyperplaneX]
+x1High = [(X[:, :1][a] if Y[a] == 1 else None) for a in range(len(Y))]
+x2High = [(X[:, 1:2][a] if Y[a] == 1 else None) for a in range(len(Y))]
+x1Low = [(X[:, :1][a] if Y[a] == 0 else None) for a in range(len(Y))]
+x2Low = [(X[:, 1:2][a] if Y[a] == 0 else None) for a in range(len(Y))]
+plt.plot(hyperplaneX, hyperplaneY, 'k', x1High, x2High, 'r^', x1Low, x2Low, 'bo')
+plt.xlabel("x1")
+plt.ylabel("x2")
+plt.show()
+'''
